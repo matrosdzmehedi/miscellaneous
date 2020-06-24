e0e26a0ce0bf91844d1852e1d20204ecb0b9bf2e
unknown <matrosdzmehedi111@gmail.com>
2020-06-24 16:16:17 +0600

    craeting folder for windows file

---
+++ Windows/lock_wallpaper.bat
@@ -0,0 +1,6 @@
+@echo off
+mkdir C:\Users\%USERNAME%\Pictures\New_picture
+cd C:\Users\%USERNAME%\Pictures\New_picture
+xcopy C:\Users\%USERNAME%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\* C:\Users\%USERNAME%\Pictures\New_picture
+ren *.* *.jpg
+%SystemRoot%\explorer.exe "C:\Users\%USERNAME%\Pictures\New_picture"
