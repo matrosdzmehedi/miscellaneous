492b09fdcca0f591a2c9f16ab8533cb5697d2e68
matrosdzmehedi <matrosdzmehedi111@gmail.com>
2021-02-19 01:51:37 +0600

    added new script and directory to show all avaible command on user system alhpabatically sorted

--- Linux/com.sh
+++ Linux/com.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+
+mkdir Commands;
+
 for i in a b c d e f g h i j k l m n o p q r s t u v w x y z
 do
 
-	ls /bin | grep -i '^'$i >> command_start_with_$i.txt
+	ls /bin | grep -i '^'$i >> ./Commands/command_start_with_$i.txt
 done 
