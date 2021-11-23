95d92b169087ec95190b44d3f510da5bb7348e4d
Matrosdzmehedi <matrosdzmehedi111@gmail.com>
2021-11-23 12:23:32 +0600

    readfile line by line,replace space with underscor

--- Linux/name.sh
+++ Linux/name.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$1" == "-h" ] ; then
-    echo "Usage: `basename $0` reading_filename  writing_filename"
+    echo "Usage: `basename $0` reading_filename  writing_filename "
     exit 0
 fi
 while IFS= read -r line
